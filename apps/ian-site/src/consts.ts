dad19edffc897a2fc4e1c0e8a8dc58dd0c4f288c
orwarhol <ian.c.slater@gmail.com>
2026-02-07 16:11:45 -0800

    Update SITE_DESCRIPTION with detailed information

--- apps/ian-site/src/consts.ts
+++ apps/ian-site/src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = "Ian Slater";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_DESCRIPTION = "Ian Slater is a writer, filmmaker, and photographer. His screenplay Hattie earned an Honorable Mention in the Fresh Voices Screenplay Competition and was a finalist at RIFF. He works in tech product leadership and lives with his wife and two children in Calabasas, California.";
